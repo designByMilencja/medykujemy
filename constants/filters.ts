fe5fdf5c4555cecda3f3645c45eca3026cd10fe4
Milena Pieńkosz <designbymilencja@gmail.com>
2024-05-12 12:25:20 +0200

    added constants

---
+++ constants/filters.ts
@@ -0,0 +1,9 @@
+export const HomePageFilters = [
+  { name: "Najnowsze", value: "newest" },
+  { name: "Najstarsze", value: "oldest" },
+];
+
+export const JobPageFilters = [
+  { name: "Pracownicy", value: "employees" },
+  { name: "Pracodawcy", value: "employers" },
+];
