50946f1dff78c281cd4fc4a41c413dd02f6da849
Milena Pieńkosz <designbymilencja@gmail.com>
2024-05-12 12:25:12 +0200

    added types

---
+++ types/index.d.ts
@@ -0,0 +1,47 @@
+export interface SidebarLink {
+  imgURL: string;
+  route: string;
+  label: string;
+}
+
+export interface Reset {
+  email: string;
+}
+
+export interface LoginData extends Reset {
+  password: string;
+}
+
+export interface UserRegister extends LoginData {
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  role: string;
+  isVerified: boolean;
+  companyName?: string;
+  licenceNumber?: string;
+  resetPasswordToken: string;
+  resetPasswordTokenExpiry: string;
+  verifyToken: string;
+  verifyTokenExpiry: string;
+  city?: string;
+  occupation?: string;
+  specialization?: string;
+  experience?: string;
+  hours?: string;
+  contractType?: string;
+  additional?: string;
+  responsibilities?: string;
+  requirements?: string;
+  brandLink?: string;
+  accept: false;
+}
+export interface ParamsProps {
+  params: { id: string };
+}
+
+export interface JobProps {
+  userDetails?: string;
+  type: "add" | "edit";
+}
