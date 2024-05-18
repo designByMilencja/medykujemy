f6524bcd4b9ae7f72d4a4fcd4b000d3bc49bb47e
Milena Pieńkosz <designbymilencja@gmail.com>
2024-05-18 16:57:05 +0200

    added types for advertisement

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -1,124 +1,138 @@
-import {IUser} from "@/models/user.model";
+import { IUser } from "@/models/user.model";
 // artykuły
 
 export interface CreateArticleParams {
-    title: string;
-    desc: string;
-    description: string;
-    image: string;
-    tags: string[];
-    path: string;
+  title: string;
+  desc: string;
+  description: string;
+  image: string;
+  tags: string[];
+  path: string;
 }
 
-
 export interface EditArticleParams {
-    articleId?: string;
-    title: string;
-    desc: string;
-    description: string;
-    image: string;
-    path: string;
+  articleId?: string;
+  title: string;
+  desc: string;
+  description: string;
+  image: string;
+  path: string;
 }
 
 export interface GetArticlesParams {
-    page?: number;
-    pageSize?: number;
-    searchQuery?: string;
-    filter?: string;
+  page?: number;
+  pageSize?: number;
+  searchQuery?: string;
+  filter?: string;
 }
 
 export interface GetArticleByIdParams {
-    articleId: string;
+  articleId: string;
 }
-
-
 
 // pracodawca
 export interface EditEmployerParams {
-    userId?: string;
-    companyName: string,
-    brandLink: string;
-    city: string,
-    occupation: string,
-    specialization: string;
-    responsibilities: string,
-    requirements: string,
-    contractType: string,
-    hours: string;
-    role: string;
-    accept: boolean;
-    additional: string | undefined | null;
-    path: string;
+  userId?: string;
+  companyName: string;
+  brandLink: string;
+  city: string;
+  occupation: string;
+  specialization: string;
+  responsibilities: string;
+  requirements: string;
+  contractType: string;
+  hours: string;
+  role: string;
+  accept: boolean;
+  additional: string | undefined | null;
+  path: string;
 }
 
 // pracownik
 export interface EditEmployeeParams {
-    userId?: string;
-    city: string,
-    occupation: string,
-    specialization: string;
-    experience: string,
-    contractType: string,
-    hours: string;
-    additional: string | undefined | null;
-    role: string;
-    accept: boolean,
-    path: string;
+  userId?: string;
+  city: string;
+  occupation: string;
+  specialization: string;
+  experience: string;
+  contractType: string;
+  hours: string;
+  additional: string | undefined | null;
+  role: string;
+  accept: boolean;
+  path: string;
 }
-
 
 // user
 
 export interface UpdateUserParams {
-    id: string;
-    updateData: Partial<IUser>;
-    path: string;
+  id: string;
+  updateData: Partial<IUser>;
+  path: string;
 }
 export interface DeleteUserParams {
-    id: string;
+  id: string;
 }
 
 export interface GetAllUsersParams {
-    page?: number;
-    pageSize?: number;
-    filter?: string;
-    searchQuery?: string; // Add searchQuery parameter
+  page?: number;
+  pageSize?: number;
+  filter?: string;
+  searchQuery?: string; // Add searchQuery parameter
 }
 
 export interface GetUserByIdParams {
-    userId: string;
+  userId: string;
 }
-
-
 
 // procedury
 export interface CreateProcedureParams {
-    title: string;
-    desc: string;
-    description: string;
-    image: string;
-    video: string;
-    sources: string[];
-    path: string;
+  title: string;
+  desc: string;
+  description: string;
+  image: string;
+  video: string;
+  sources: string[];
+  path: string;
 }
-
 
 export interface EditProcedureParams {
-    procedureId?: string;
-    title: string;
-    desc: string;
-    description: string;
-    image: string;
-    video: string;
-    path: string;
+  procedureId?: string;
+  title: string;
+  desc: string;
+  description: string;
+  image: string;
+  video: string;
+  path: string;
 }
 export interface GetProceduresParams {
-    page?: number;
-    pageSize?: number;
-    searchQuery?: string;
-    filter?: string;
+  page?: number;
+  pageSize?: number;
+  searchQuery?: string;
+  filter?: string;
 }
 
 export interface GetProcedureByIdParams {
-    procedureId: string;
+  procedureId: string;
+}
+
+// ogłoszenie
+
+export interface UpdateNoticeParams {
+  ownerId: string;
+  updateData: Partial<IVotes>;
+  path: string;
+}
+
+export interface GetNoticeByIdParams {
+  ownerId: string;
+}
+
+export interface DeleteNoticeParams {
+  ownerId: string;
+}
+
+export interface RemoveVoteParams {
+  noticeId: string;
+  userId: string;
 }
