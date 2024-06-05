44d023ebe11af61829d773162fa90c2ae8cc557c
Milena Pieńkosz <designbymilencja@gmail.com>
2024-06-05 16:49:42 +0200

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,17 @@
-export {default} from 'next-auth/middleware'
-export const config = {matcher: ["/jobs", "/procedures", "/account"]}
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!session) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/sign-in";
+    url.search = `callbackUrl=${req.nextUrl.pathname}`;
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = { matcher: ["/jobs", "/procedures", "/account"] };
