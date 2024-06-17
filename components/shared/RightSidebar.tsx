c4fbceb99ca88612e57631dfcf633f7b30e1b798
Milena Pieńkosz <designbymilencja@gmail.com>
2024-06-17 08:13:30 +0200

    limited tags and articles

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,12 +6,14 @@ import { getTopPopularTags } from "@/lib/actions/tag.action";
 const RightSidebar = async () => {
   const articles = await getLastAddedArticles();
   const tags = await getTopPopularTags();
+  const displayedArticles = articles.slice(0, 5);
+  const displayedTags = tags.slice(0, 10);
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[300px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-lg:hidden xl:w-[400px]">
       <div>
         <h3 className="h3-bold text-dark200_light900">Ostatnio dodane</h3>
         <div className="mt-7 flex w-full flex-col gap-4">
-          {articles.map((article) => (
+          {displayedArticles.map((article) => (
             <Link
               key={article._id}
               href={`/articles/${article._id}`}
@@ -27,7 +29,7 @@ const RightSidebar = async () => {
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popularne tematy</h3>
         <div className="mt-7 flex flex-wrap gap-3">
-          {tags.map((tag) => (
+          {displayedTags.map((tag) => (
             <Tag key={tag._id} _id={tag._id} name={tag.name} />
           ))}
         </div>
