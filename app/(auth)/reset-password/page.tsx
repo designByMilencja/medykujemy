df2e22c7d3a33afbc099d90266b057d2ce1dff9d
Milena Pieńkosz <designbymilencja@gmail.com>
2024-06-18 21:15:19 +0200

    changed react quill for dynamic events and procedure

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -2,11 +2,11 @@ import ResetPasswordForm from "@/components/forms/auth/ResetPasswordForm";
 import React, { Suspense } from "react";
 
 const Reset = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ResetPasswordForm />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ResetPasswordForm />
+        </Suspense>
+    );
 };
 
 export default Reset;
