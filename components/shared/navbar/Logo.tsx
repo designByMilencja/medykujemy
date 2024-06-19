0cddf04186a24fe705072546ae4ee56f2502ce9c
Milena Pieńkosz <designbymilencja@gmail.com>
2024-06-19 21:16:56 +0200

    changed logo

--- components/shared/navbar/Logo.tsx
+++ components/shared/navbar/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
     <Link href="/" className="flex items-center gap-1">
       <Image src="/assets/icons/logo.svg" alt="Logo" width={53} height={53} />
       <p className="h2-bold font-poppins text-accent-blue  dark:text-light-900 max-sm:hidden">
-        Medykujemy.pl
+        MEDY<span className="text-primary-500">kujemy.pl</span>
       </p>
     </Link>
   );
