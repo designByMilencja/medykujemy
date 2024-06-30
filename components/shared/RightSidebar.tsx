40f9d266b617adb78afe1ed11dfebeee9eae3dd2
Milena Pieńkosz <designbymilencja@gmail.com>
2024-06-30 17:48:36 +0200

    changed titles of section

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -11,7 +11,7 @@ const RightSidebar = async () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[300px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-lg:hidden xl:w-[400px]">
       <div>
-        <h3 className="h3-bold text-dark200_light900">Ostatnio dodane</h3>
+        <h3 className="h3-bold text-dark200_light900">Ostatnie artykuły</h3>
         <div className="mt-7 flex w-full flex-col gap-4">
           {displayedArticles.map((article) => (
             <Link
@@ -27,7 +27,7 @@ const RightSidebar = async () => {
         </div>
       </div>
       <div className="mt-16">
-        <h3 className="h3-bold text-dark200_light900">Popularne tematy</h3>
+        <h3 className="h3-bold text-dark200_light900">Kategorie artykułów</h3>
         <div className="mt-7 flex flex-wrap gap-3">
           {displayedTags.map((tag) => (
             <Tag key={tag._id} _id={tag._id} name={tag.name} />
