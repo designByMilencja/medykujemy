c87a38a47fd1d814db08c750a0f020fc93bbe66f
Milena Pieńkosz <designbymilencja@gmail.com>
2024-07-20 22:05:51 +0200

    added secrets env mongodb

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,14 @@
-import Article from "@/components/articles/Article";
+// import Article from "@/components/articles/Article";
 import Filters from "@/components/home/Filters";
 import Filter from "@/components/shared/Filter";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { HomePageFilters } from "@/constants/filters";
 import { Metadata } from "next";
 import Link from "next/link";
-import { getArticles } from "@/lib/actions/article.action";
+// import { getArticles } from "@/lib/actions/article.action";
 import { getServerSession } from "next-auth";
 import { options } from "@/app/(auth)/api/auth/[...nextauth]/options";
-import Empty from "@/components/shared/EmptySection";
+// import Empty from "@/components/shared/EmptySection";
 import { SearchParamsProps } from "@/types";
 import Pagination from "@/components/shared/Pagination";
 
@@ -22,10 +22,10 @@ const Home = async ({ searchParams }: SearchParamsProps) => {
   const session = await getServerSession(options);
   const admin = session?.user?.role === "admin";
 
-  const articles = await getArticles({
-    searchQuery: searchParams.q,
-    filter: searchParams.filter,
-  });
+  // const articles = await getArticles({
+  //   searchQuery: searchParams.q,
+  //   filter: searchParams.filter,
+  // });
   // @ts-ignore
   return (
     <>
@@ -56,24 +56,24 @@ const Home = async ({ searchParams }: SearchParamsProps) => {
         />
       </div>
       <Filters filters={HomePageFilters} />
-      <div className="mt-1 flex w-full flex-col gap-6">
-        {articles.articles.length > 0 ? (
-          articles.articles.map((article) => (
-            <Article
-              key={article._id}
-              desc={article.desc}
-              _id={article._id}
-              title={article.title}
-              src={article.image}
-              tags={article.tags}
-              createdAt={article.createdAt}
-              role={admin}
-            />
-          ))
-        ) : (
-          <Empty title="Brak artykułów" />
-        )}
-      </div>
+      {/* <div className="mt-1 flex w-full flex-col gap-6"> */}
+      {/*  {articles.articles.length > 0 ? ( */}
+      {/*    articles.articles.map((article) => ( */}
+      {/*      <Article */}
+      {/*        key={article._id} */}
+      {/*        desc={article.desc} */}
+      {/*        _id={article._id} */}
+      {/*        title={article.title} */}
+      {/*        src={article.image} */}
+      {/*        tags={article.tags} */}
+      {/*        createdAt={article.createdAt} */}
+      {/*        role={admin} */}
+      {/*      /> */}
+      {/*    )) */}
+      {/*  ) : ( */}
+      {/*    <Empty title="Brak artykułów" /> */}
+      {/*  )} */}
+      {/* </div> */}
       <div className="mt-10">
         <Pagination isNext={true} pageNumber={12} />
       </div>
